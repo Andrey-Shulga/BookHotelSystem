7c7dffa27940e2ec194f559f0977bd1b28db7939
Andrey Shulga <solyariz@gmail.com>
2016-12-23 18:03:14 +0600

    Added: ErrorHandlerServlet, forwarding on special error page for error code.

---
+++ src/main/java/com/epam/as/bookhotel/servlet/ErrorHandlerServlet.java
@@ -0,0 +1,40 @@
+package com.epam.as.bookhotel.servlet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ErrorHandlerServlet", urlPatterns = "/ErrorHandler")
+public class ErrorHandlerServlet extends HttpServlet {
+
+    private static final Logger logger = LoggerFactory.getLogger(ErrorHandlerServlet.class);
+    private static final String PATH_TO_JSP = "/WEB-INF/jsp/";
+    private static final String FILE_JSP = ".jsp";
+    private static final String ERROR_STATUS_CODE_ATTRIBUTE = "javax.servlet.error.status_code";
+    private static final String ERROR_403_PAGE = "error403";
+    private static final String ERROR_500_PAGE = "error500";
+    private static final int ERROR_500_CODE = 500;
+    private static final int ERROR_403_CODE = 403;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Integer errorStatusCode = (Integer) req.getAttribute(ERROR_STATUS_CODE_ATTRIBUTE);
+        logger.debug("Received Error with code {}, will be forwarded to proper error page", errorStatusCode);
+        if (errorStatusCode == ERROR_403_CODE)
+            req.getRequestDispatcher(PATH_TO_JSP + ERROR_403_PAGE + FILE_JSP).forward(req, resp);
+        if (errorStatusCode == ERROR_500_CODE)
+            req.getRequestDispatcher(PATH_TO_JSP + ERROR_500_PAGE + FILE_JSP).forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/error500.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" isErrorPage="true" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+<%@taglib prefix="t" tagdir="/WEB-INF/tags" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<fmt:setBundle basename="lang"/>
+
+<fmt:message key="error.title" var="title"/>
+
+<t:genericpage title="${title}">
+    <jsp:body>
+
+        <b><fmt:message key="access.error.message.label"/></b> <fmt:message key="database.connection.failure.msg"/><br>
+        <b> <fmt:message key="access.error.code.label"/></b> ${pageContext.errorData.statusCode}<br>
+        <hr>
+
+        <b><fmt:message key="internal.error.admin.message"/></b> <br><br>
+        <b>Exception:</b> ${pageContext.exception}<br>
+        <b>Message: </b>${pageContext.exception.message}<br>
+        <b>Stacktrace: </b><br>
+        <c:forEach var="trace" items="${pageContext.exception.stackTrace}">
+            ${trace} <br>
+        </c:forEach>
+
+    </jsp:body>
+</t:genericpage>
